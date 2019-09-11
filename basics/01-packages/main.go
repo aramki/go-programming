ff4b9ab6ee6580252eaf792105de761eacd8db44
Ramakrishnan A <aramki@gmx.com>
2019-09-11 08:43:31 +0530

    introducing packages

---
+++ basics/01-packages/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main () {
+	fmt.Println("Inside Main")
+
+	fmt.Println("Printing Wishes")
+	wishes()
+}
---
+++ basics/01-packages/wishes.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func wishes() {
+	fmt.Print("Wishing a very good day")
+}
